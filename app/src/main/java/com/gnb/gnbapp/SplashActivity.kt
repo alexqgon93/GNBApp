509b27976d230d318d695220adf95be8410bb1b0
aeeu <alexandre.esquerdo@gft.com>
2021-02-18 19:35:48 +0100

    added dependencies, splash screen and new icon's App

---
+++ app/src/main/java/com/gnb/gnbapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.gnb.gnbapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+    companion object{
+        const val SPLASH_TIME_OUT: Long = 3000 // duration of 1 second
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
